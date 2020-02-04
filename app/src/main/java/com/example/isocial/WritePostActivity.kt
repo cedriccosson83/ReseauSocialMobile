28c9fec9479f7b357a7db57e700d54c117f8ac94
cedriccosson83 <cedric.cosson@etu.univ-amu.fr>
2020-02-04 17:25:15 +0200

    Publication V1.0

--- app/src/main/java/com/example/isocial/WritePostActivity.kt
+++ app/src/main/java/com/example/isocial/WritePostActivity.kt
@@ -2,9 +2,15 @@ package com.example.isocial
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_write.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 class WritePostActivity : AppCompatActivity() {
 
@@ -14,8 +20,16 @@ class WritePostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
-        // Initialize Firebase Auth
+
         auth = FirebaseAuth.getInstance()
+        publishBTN.setOnClickListener{
+            val userid = auth.currentUser?.uid
+            if (userid != null){
+                newPost(userid, Date().toString(), publish_field.text.toString())
+            } else {
+                Toast.makeText(this, "Utilisateur non trouvé", Toast.LENGTH_LONG)
+            }
+        }
         //newPost("CedricId123", "04/02/2020", "Bonjour je suis un message écrit par" +
         //        "Cédric cosson et je fais une grande quantité de caractères")
     }
