c1d214af387851e51ff2620a4c293fbd632d6434
FlorentRicciardi <florent.ricciardi@isen.yncrea.fr>
2020-02-04 10:53:41 +0100

    ADD FeedActivity

---
+++ app/src/main/java/com/example/isocial/FeedActivitity.kt
@@ -0,0 +1,12 @@
+package com.example.isocial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivitity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
