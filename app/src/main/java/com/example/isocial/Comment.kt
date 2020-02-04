58e087c22ca686538b73cd00b0a3d33f4674e721
cedriccosson83 <cedric.cosson@etu.univ-amu.fr>
2020-02-04 17:00:00 +0200

    Data class "Posts" & "Comment"

---
+++ app/src/main/java/com/example/isocial/Comment.kt
@@ -0,0 +1,22 @@
+package com.example.isocial
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Comment (
+    var userid: String = "",
+    var postId: String = "",
+    var date: String? = "",
+    var content: String? = "",
+    var likedBy: ArrayList<User> = ArrayList()
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "userid" to userid,
+            "postId" to postId,
+            "date" to date,
+            "content" to content,
+            "likedBy" to likedBy
+        )
+    }
+}
---
+++ app/src/main/java/com/example/isocial/Post.kt
@@ -0,0 +1,22 @@
+package com.example.isocial
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Post (
+    var userid: String = "",
+    var postid: String = "",
+    var date: String? = "",
+    var content: String? = "",
+    var likes: ArrayList<User> = ArrayList(),
+    var comments: ArrayList<Comment> = ArrayList()
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "userid" to userid,
+            "date" to date,
+            "content" to content,
+            "likes" to likes
+        )
+    }
+}
