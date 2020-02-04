60e8beea919c2f3b62fc7530d15324be5279d79a
Luciegaire <lucie83140@hotmail.fr>
2020-02-04 16:04:33 +0300

    login inscription

--- app/src/main/java/com/example/isocial/LoginActivity.kt
+++ app/src/main/java/com/example/isocial/LoginActivity.kt
@@ -2,31 +2,75 @@ package com.example.isocial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
-
-    private val au
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
-        lateinit var auth : FirebaseAuth
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
+
+        signinbutton.setOnClickListener {
+            signin()
+        }
+
+        signupbutton.setOnClickListener {
+            signup()
+        }
     }
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
+    fun signup() {
+        auth.createUserWithEmailAndPassword(emaileditText.text.toString(), passwordeditText.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign up success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign up fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+    fun signin() {
+        auth.signInWithEmailAndPassword(emaileditText.text.toString(), passwordeditText.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "connect user email:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "connect user email:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "E-mail ou mot de passe incorrect", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    //Change UI according to user data.
+    fun updateUI(account: FirebaseUser?) {
+        if (account != null ) {
+            Toast.makeText(this, "Signed in " + account?.uid, Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, "You have an account", Toast.LENGTH_LONG).show()
+        }
     }
 
-        private lateinit var auth: FirebaseAuth
-        // Initialize Firebase Auth
-        auth = FirebaseAuth.getInstance()
-    }
+
 }
